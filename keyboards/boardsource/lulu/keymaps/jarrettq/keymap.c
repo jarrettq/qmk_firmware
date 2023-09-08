d807d595f5146e1fbf08092906ba49610db90550
jquanhin <cujo.quan@gmail.com>
2023-09-08 13:08:20 -0400

    First tapdance test addition to the config

--- keyboards/boardsource/lulu/keymaps/jarrettq/keymap.c
+++ keyboards/boardsource/lulu/keymaps/jarrettq/keymap.c
@@ -13,6 +13,14 @@ enum layers {
 #define RAISE MO(_RAISE)
 #define LOWER MO(_LOWER)
 
+enum {
+  TD_C
+};
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+  [TD_C] = ACTION_TAP_DANCE_DOUBLE(KC_C, LCTL(KC_C))
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  
 /* QWERTY
@@ -34,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_BSPC,
   KC_TAB,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_B,                      KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,  KC_DEL,
   KC_BSPC,  KC_A,   KC_R,    KC_S,    KC_T,    KC_G,                      KC_M,    KC_N,    KC_E,    KC_I,    KC_O,     KC_QUOT,
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_D,    KC_V,  KC_LBRC,  KC_RBRC,  KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
+  KC_LSFT,  KC_Z,   KC_X,    TD(TD_C),KC_D,    KC_V,  KC_LBRC,  KC_RBRC,  KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
                           KC_LALT,    KC_LGUI, LOWER, KC_LCTL,  KC_SPC,   KC_LSFT, KC_ENT, RAISE
 ),
 /* LOWER

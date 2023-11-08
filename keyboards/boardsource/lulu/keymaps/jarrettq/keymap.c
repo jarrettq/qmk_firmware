4b8bbe633930a1bdfd55f876ef37a7ddd9481660
jquanhin <cujo.quan@gmail.com>
2023-11-07 20:55:15 -0500

    updated the gaming layer to reflect changes made in via  Please enter the commit message for your changes. Lines starting

--- keyboards/boardsource/lulu/keymaps/jarrettq/keymap.c
+++ keyboards/boardsource/lulu/keymaps/jarrettq/keymap.c
@@ -119,7 +119,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // |-------+-------+-------+-------+-------+-------|                    |-------+-------+-------+-------+---------+--------|
     KC_BSPC, KC_A,    KC_A,  KC_S,   KC_D,   KC_F,                        KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN, KC_QUOT,
 // |-------+-------+-------+-------+-------+-------|        |   |       |-------+-------+-------+-------+---------+--------|
-    KC_LSFT, KC_LEFT, KC_X,  KC_C,   KC_V, KC_LALT, KC_LBRC,     KC_RBRC, KC_N,   KC_M,  KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+    KC_LSFT, KC_LEFT, KC_Z,  KC_X,   KC_C, KC_LALT, KC_LBRC,     KC_RBRC, KC_N,   KC_M,  KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
 // `-----------------------------------------------/       /     \       \------------------------------------------------'
                       LOWER, KC_LEFT, KC_RIGHT, KC_SPC,              KC_SPC,  KC_LSFT,  KC_ENT, RAISE
 //                  `--------+-------+-------+----------'           '--------+--------+-------+-------'
